076bc6c53275dcc69830dc5ac2361939c5bb48f8
Meghan Bock <meghan.bock@gmail.com>
2021-10-04 01:19:33 -0500

    set up typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,32 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID!
+        authors: []
+        title: String
+        description: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookId: ID!): User
+        removeBook(bookId: ID!): User
+    }
+`;
