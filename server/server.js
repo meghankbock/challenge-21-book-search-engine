631a87d6f035c02c376d4e6424b1c39eb5796b07
Meghan Bock <meghan.bock@gmail.com>
2021-10-04 00:54:36 -0500

    install dependencies, set up schema folder + create initial files, update server.js (add apollo server and remove routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const { authMiddleware } = require("./utils/auth");
+// const routes = require('./routes');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware,
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground({
+      }),
+    ]
+  });
+
+  await server.start();
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +38,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
